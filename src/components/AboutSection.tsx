3f029a55b06c854b3cf434652e7d77c59ebb10e2
meiryankotarget-maker <meiryanko.target@gmail.com>
2026-03-03 17:55:53 +0200

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -18,8 +18,8 @@ const orbitRadius = 215;
 const mobileOrbitRadius = 150;
 const mobileCompassSize = 190;
 const mobileContainerSize = 320;
-const mobileCenterX = 140;
-const mobileCenterY = 140;
+const mobileCenterX = 135;
+const mobileCenterY = 150;
 const AboutSection = () => {
   const [needleRotation, setNeedleRotation] = useState(0);
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
